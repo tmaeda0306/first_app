837cb0efae51d2485a13fd52ce157f464b6b8492
EC2 Default User <ec2-user@ip-172-31-17-80.ap-northeast-1.compute.internal>
2021-12-24 08:44:18 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+    def index
+        @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+    end
+    
+    def new
+    end
+    
+    def create
+        Post.create(content: params[:content])
+    end
+end
